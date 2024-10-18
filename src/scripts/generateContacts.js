0601de3449be0ec5706aeb308cb4a9c498d3f71b
Vlad <vladonik.909@gmail.com>
2024-10-18 14:45:19 +0300

    100%

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,13 @@
-const generateContacts = async (number) => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const generateContacts = async (number) => {
+  const contactsList = await readContacts();
+  const newContacts = Array(number)
+    .fill()
+    .map(() => createFakeContact());
+  await writeContacts([...contactsList, ...newContacts]);
+};
 
 generateContacts(5);
